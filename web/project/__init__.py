32e7a1d0ac52633c5db4442bc7205fb0d4adcd07
Carl W. Handlin <cwallaceh@gmail.com>
2019-02-12 12:39:41 -0600

    Added web files

---
+++ web/project/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+from project import views, models
